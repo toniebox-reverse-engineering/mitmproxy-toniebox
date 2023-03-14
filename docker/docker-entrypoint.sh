8c561a5a5ef80b052cabfbc6a58f151c7cf6f699
SciLor <scilor@github.com>
2023-03-14 07:36:38 +0000

    Add SSLKEYLOGFILE to entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -45,5 +45,6 @@ if [ ! -f "$MITMPROXY_CERT_PATH/mitmproxy-ca.pem" ]; then
   echo "...created!"
 fi
 
+echo "SSLKEYLOGFILE=$SSLKEYLOGFILE"
 echo "$@"
-exec "$@"
+exec env SSLKEYLOGFILE=$SSLKEYLOGFILE "$@"
