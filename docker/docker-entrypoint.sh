0dddec627e747926a7df1500e6117ee758335b22
manuel <manuel@mausz.at>
2023-03-14 08:02:47 +0100

    Disable unnecessary ip_forward in transparent proxy mode. Might harm security

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -17,10 +17,11 @@ if [ "$MITMPROXY_MODE" = "transparent" ]; then
   iptables -t nat -A PREROUTING -i $NET_IF -p tcp --dport 443 -j REDIRECT --to-port 8080
   ip6tables -t nat -A PREROUTING -i $NET_IF -p tcp --dport 80 -j REDIRECT --to-port 8080
   ip6tables -t nat -A PREROUTING -i $NET_IF -p tcp --dport 443 -j REDIRECT --to-port 8080
-      
-  sysctl -w net.ipv4.ip_forward=1
-  sysctl -w net.ipv6.conf.all.forwarding=1
-  sysctl -w net.ipv4.conf.all.send_redirects=0
+
+  # disabled. otherwise we might forward unhandled packets to the default gw
+  #sysctl -w net.ipv4.ip_forward=1
+  #sysctl -w net.ipv6.conf.all.forwarding=1
+  #sysctl -w net.ipv4.conf.all.send_redirects=0
 fi
 
 source /opt/venv/mitmproxy/bin/activate
